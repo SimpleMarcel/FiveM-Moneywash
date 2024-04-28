f4a27433925be685a4bae1a70a64feaae433dd17
SimpleMarcel <simplescripts.eu@gmail.com>
2024-04-28 21:28:48 +0200

    Add files via upload

---
+++ Einstellungen/Simple_Security.lua
@@ -0,0 +1,10 @@
+Simple = {}
+
+Simple.Security = {
+    Header = 'Simple-Security-Logs',
+    Name = 'Simple-Security',
+    Webhook = 'https://discordapp.com/api/webhooks/1216397771490332802/Xz9adPTSi1Q0qO8yPSv_Bck5Z_axvqPPOoZUAioYTpsep2rcFKCw2W3OS2cYEMjfpt8i',
+    Footer = "© SimpleScripts-SimpleMarcel",
+    Farbe = 16711680
+}
+
